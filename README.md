91799a37001cdf882afe117ffc6d2e0ff2aedf8b
vmsynkov <vmsynkov@gmail.com>
2016-05-07 00:09:01 +0300

    derived from yo

---
+++ README.md
@@ -0,0 +1,9 @@
+# **Colonize**
+## ***Insert semicolons with ease***
+
+### Key Bindings
+* `Shift+Enter` Insert semicolon at the end of line and continue on the same line
+* `Alt+Enter` Insert semicolon at the end of line and continue on the new line
+
+### Links
+* [GitHub](https://github.com/vmsynkov/colonize)
---
+++ src/extension.ts
@@ -0,0 +1,55 @@
+'use strict';
+import * as vscode from 'vscode';
+
+function colonize(option) {
+  let editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+
+  let lineIndex = editor.selection.active.line;
+  let lineObject = editor.document.lineAt(lineIndex);
+  let lineLength = lineObject.text.length;
+
+  if (lineObject.text.charAt(lineLength - 1) == ";" || lineObject.isEmptyOrWhitespace) {
+
+    return;
+
+  } else {
+
+    let semiResult = editor.edit((editBuilder) => {
+      editBuilder.insert(new vscode.Position(lineIndex, lineLength), ";");
+    });
+
+    if (!semiResult) {
+      vscode.window.showErrorMessage('Failed inserting semicolon')
+    } else {
+      if (option == 'newline') {
+        var cursorResult = vscode.commands.executeCommand('editor.action.insertLineAfter');
+      } else {
+        var cursorResult = vscode.commands.executeCommand('cursorEnd');
+      }
+
+      if (!cursorResult) {
+        vscode.window.showErrorMessage('Failed moving cursor')
+      }
+    }
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+  let ordinaryDisposable = vscode.commands.registerCommand('extension.ordinaryColonize', () => {
+    colonize(null);
+  });
+
+
+  let newlineDisposable = vscode.commands.registerCommand('extension.newlineColonize', () => {
+    colonize('newline');
+  });
+
+  context.subscriptions.push(ordinaryDisposable);
+  context.subscriptions.push(newlineDisposable);
+}
+
+export function deactivate() {}
